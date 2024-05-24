a781140178f16c70de43ed4ecaba47190c012cd7
angWindy <anthang2003.nd@gmail.com>
2024-05-25 03:46:19 +0700

    Upload data crawl

--- website/models.py
+++ website/models.py
@@ -11,9 +11,12 @@ class User(db.Model, UserMixin):
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), unique=True)
-    price = db.Column(db.Integer)
-    description = db.Column(db.String(500))
-    image = db.Column(db.String(150))
+    price = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    review_count = db.Column(db.Integer)
+    description = db.Column(db.String(1000))
+    image = db.Column(db.String(150), nullable=False)
+    category = db.Column(db.String(150), nullable=False)
 
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
