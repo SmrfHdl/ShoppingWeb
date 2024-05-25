8039a03e84a405fa73a13506c6803fcfa323f80b
angWindy <anthang2003.nd@gmail.com>
2024-05-26 01:03:22 +0700

    Update home

--- website/pd.py
+++ website/pd.py
@@ -1,10 +1,19 @@
 from flask import Blueprint, render_template, abort
 from website.models import Product, db
+# Import for Recommendation System
+# from RecommendationSystem.CBCF_function import get_CF_data
+# from RecommendationSystem.collaborative_filtering import CF
 
 pd_bp = Blueprint('pd', __name__)
 
 @pd_bp.route("/products/<string:product_url>")
 def home(product_url):
+
+    # data = get_CF_data("instance\database.db") #ub.base là file chứa rating của một số user về các sản phẩm. mỗi user đã rating ít nhất 10 sản phẩm
+    # test = CF(data,5)
+    # test.fit()
+
+
     product = db.session.query(Product).filter_by(url=product_url).first()
     if not product:
         print("Product not found")
--- website/views.py
+++ website/views.py
@@ -1,9 +1,16 @@
 from flask import Blueprint, render_template
 from flask_login import login_user, login_required, current_user
 
+from website.models import Product
+
 views = Blueprint('views', __name__)
 
 @views.route('/')
 # @login_required
 def home():
-    return render_template("home.html")
+    # Lấy ra 4 sản phẩm có lượng review_count cao nhất
+    top_reviewed_products = Product.query.order_by(Product.review_count.desc()).limit(4).all()
+
+    # Lấy ra 4 sản phẩm có rating cao nhất
+    top_rated_products = Product.query.order_by(Product.rating.desc()).limit(4).all()
+    return render_template("home.html", top_rated_products = top_rated_products, top_reviewed_products = top_reviewed_products)
